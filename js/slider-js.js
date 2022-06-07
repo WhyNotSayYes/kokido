5ce560d99f958b0e4a7cfc5cc2439152068a3409
WhyNotSayYes <bolgarsky.iu@gmail.com>
2022-06-07 22:30:20 +0300

    page__slider v.1

---
+++ js/slider-js.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.slider-big').slick({
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        dots: false,
+        draggable: false,
+        swipe: false,
+    });
+    $('.slider-text').slick({
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        dots: false,
+        draggable: false,
+        swipe: false,
+    });
+    $('.slider-min').slick({
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        centerMode: true,
+        asNavFor: '.slider-big,.slider-text',
+        autoplay: false,
+        autoplaySpeed: 1500,
+    });
+})
